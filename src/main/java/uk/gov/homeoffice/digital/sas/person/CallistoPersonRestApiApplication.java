d6eaf3bc725025cdc1584bd03e8c61b155efd881
Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>
2023-02-03 09:34:35 +0000

    Exclude datasource auto config for now. Rename notpord kube secrets to be consistent

--- src/main/java/uk/gov/homeoffice/digital/sas/person/CallistoPersonRestApiApplication.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/person/CallistoPersonRestApiApplication.java
@@ -2,8 +2,10 @@ package uk.gov.homeoffice.digital.sas.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// TODO: remove exclusion once database is setup
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CallistoPersonRestApiApplication {
   public static void main(String[] args) {
     SpringApplication.run(CallistoPersonRestApiApplication.class, args);
