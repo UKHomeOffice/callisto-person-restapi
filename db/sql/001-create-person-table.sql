c48532825f9c361da15ea225ad5860115e9381ba
Seweryn <seweryn.michota@digital.homeoffice.gov.uk>
2023-02-06 17:55:39 +0000

    Add test data

--- db/sql/001-create-person-table.sql
+++ db/sql/001-create-person-table.sql
@@ -10,3 +10,14 @@ CREATE TABLE IF NOT EXISTS person.person (
     fte_value NUMERIC (1,1) NOT NULL,
     terms_and_conditions VARCHAR(30) NOT NULL
     );
+
+-- just test data, will be removed
+INSERT INTO person.person (id,tenant_id,version,first_name,last_name,fte_value,terms_and_conditions ) VALUES
+   ('00000000-0000-0000-0000-000000000001',
+    '00000000-0000-0000-0000-000000000000',
+    1,
+    'Adam',
+    'Smith',
+    0.5,
+    'PRE-MODERNISED'
+    );
