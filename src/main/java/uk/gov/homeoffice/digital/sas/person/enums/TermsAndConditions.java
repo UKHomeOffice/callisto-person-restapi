525140675ad32ec5e8fbcb45b6265e85203c4218
Seweryn <seweryn.michota@digital.homeoffice.gov.uk>
2023-02-09 11:04:15 +0000

    Updated docker compose yml

--- src/main/java/uk/gov/homeoffice/digital/sas/person/enums/TermsAndConditions.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/person/enums/TermsAndConditions.java
@@ -1,9 +1,9 @@
 package uk.gov.homeoffice.digital.sas.person.enums;
 
-
 public enum TermsAndConditions {
   MODERNISED("MODERNISED"),
   PRE_MODERNISED("PRE_MODERNISED");
+
   private final String stringValue;
 
   TermsAndConditions(final String s) {
