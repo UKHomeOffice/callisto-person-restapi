2d76c77c76dcc2f72dd8e9b5c6bf46b6c21f2877
Seweryn <seweryn.michota@digital.homeoffice.gov.uk>
2023-02-13 14:02:47 +0000

    Remove enum string values

--- src/main/java/uk/gov/homeoffice/digital/sas/person/enums/TermsAndConditions.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/person/enums/TermsAndConditions.java
@@ -1,18 +1,9 @@
 package uk.gov.homeoffice.digital.sas.person.enums;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 @AllArgsConstructor
-@Getter
 public enum TermsAndConditions {
-  MODERNISED("MODERNISED"),
-  PRE_MODERNISED("PRE_MODERNISED");
-
-  private final String stringValue;
-
-  @Override
-  public String toString() {
-    return stringValue;
-  }
+  MODERNISED,
+  PRE_MODERNISED;
 }
