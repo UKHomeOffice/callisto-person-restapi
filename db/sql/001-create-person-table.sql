d2b9b2d12fba3221e3d9e35f27c9c3f9e76257b7
Sev <96620703+sewerynmi@users.noreply.github.com>
2023-02-14 15:34:31 +0000

    EAHW 2572 - Person Entity Creation (#3)
    * Add test data
    
    * Create Liquibase and corresponding Dockerfile
    
    * Create Liquibase changelog to create a Person table
    
    * Create Person entity
    
    * Create basic BDD tests
    
    * Remove TestController
    
    * Lint fix Decimal import position
    
    * Temp. downgrade Spring Boot version to 2.7.0
    
    * Updated docker-compose.yml to use with LocalDev
    
    * Updated fteValue precision and BDD test
    
    * Updated openapi.yaml
    
    * Updated to Spring Boot 3
    
    * Updated BDD tests (CRUD)
    
    * Update JPA Rest version to 0.1.0
    
    * Updated BDD test (Delete Person)

---
+++ db/sql/001-create-person-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS person.person (
+    id VARCHAR(36)  PRIMARY KEY,
+    tenant_id VARCHAR(36) NOT NULL,
+    version INT NOT NULL default 1,
+    first_name VARCHAR(50) NOT NULL ,
+    last_name VARCHAR(50) NOT NULL,
+    fte_value NUMERIC (5,4) NOT NULL,
+    terms_and_conditions TEXT CHECK (terms_and_conditions IN ('MODERNISED', 'PRE_MODERNISED'))
+);
--- src/main/java/uk/gov/homeoffice/digital/sas/person/CallistoPersonRestApiApplication.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/person/CallistoPersonRestApiApplication.java
@@ -2,10 +2,8 @@ package uk.gov.homeoffice.digital.sas.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// TODO: remove exclusion once database is setup
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class CallistoPersonRestApiApplication {
   public static void main(String[] args) {
     SpringApplication.run(CallistoPersonRestApiApplication.class, args);
