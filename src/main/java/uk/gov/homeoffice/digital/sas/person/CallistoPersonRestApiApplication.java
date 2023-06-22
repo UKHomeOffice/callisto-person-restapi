39fd22c34a03e0f6c6f47cd1ef7e4e295a5fff92
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2023-06-22 13:39:55 +0100

    fix checkstyle

--- src/main/java/uk/gov/homeoffice/digital/sas/person/CallistoPersonRestApiApplication.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/person/CallistoPersonRestApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"uk.gov.homeoffice.digital.sas.kafka.producer",
-"uk.gov.homeoffice.digital.sas.kafka.transactionsync"})
+                "uk.gov.homeoffice.digital.sas.kafka.transactionsync"})
 public class CallistoPersonRestApiApplication {
   public static void main(String[] args) {
     SpringApplication.run(CallistoPersonRestApiApplication.class, args);
